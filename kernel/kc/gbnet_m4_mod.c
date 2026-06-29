3ec738b1f1489a059d29c903f75695089b6d8777
Salvatore Bognanni <salvogendut@pm.me>
2026-06-29 22:39:33 +0200

    Preserve video mode in M4 network calls

--- kernel/kc/gbnet_m4_mod.c
+++ kernel/kc/gbnet_m4_mod.c
@@ -59,8 +59,12 @@ void m4_begin(void) __naked
         push    hl
         push    ix
         push    iy
+        call    #0xBC11        ; SCR_GET_MODE -> A = current screen mode.
+        and     #0x03
+        or      #0x84          ; upper ROM on, lower ROM off, preserve mode.
         di
-        ld      bc,#0x7F85
+        ld      b,#0x7F
+        ld      c,a
         out     (c),c
         ld      bc,#0xDF00
         ld      a,#6
